9596b1c06b20eb7fda55c0506dd20dac8987c22f
Afuza <theafuza@gmail.com>
2020-11-16 09:21:01 +0700

    added database connection

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ require("dotenv/config");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARE
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+// DATABASE
+const db = require("./config/database");
 
+// CONNECT TO DATABASE
+db.authenticate()
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+// START SERVER
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
