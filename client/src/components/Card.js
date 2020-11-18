8af08339d370c40057bdf7e8d9a824c8345d02e5
Afuza <theafuza@gmail.com>
2020-11-18 15:44:17 +0700

    renamed InfoCard to Card, created SmallButton

---
+++ client/src/components/Card.js
@@ -0,0 +1,155 @@
+import React from "react";
+import styled from "styled-components";
+
+import Button from "./Button";
+import SmallButton from "./SmallButton";
+
+const CardComp = styled.div`
+  width: 280px;
+  background-color: white;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  font-size: 19px;
+  text-transform: uppercase;
+  padding: ${(props) => (props.text ? "20px" : "18px 20px")};
+  padding-bottom: ${(props) => (props.text ? "0px" : "16px")};
+  border-bottom: ${(props) => (props.text ? "" : "1px solid #ddd")};
+  font-weight: 500;
+  margin: 0;
+  box-sizing: border-box;
+`;
+
+const Desc = styled.p`
+  font-size: 16px;
+  line-height: 1.5em;
+  padding: 15px 20px;
+  padding-bottom: 0;
+  margin: 0;
+`;
+
+const List = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ListItem = styled.li`
+  width: 100%;
+  font-size: 16px;
+  list-style: none;
+  border-bottom: ${(props) => (props.noBorder ? "" : "1px solid #ddd")};
+  padding: 14px 20px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  :hover {
+    button {
+      opacity: 1;
+    }
+  }
+`;
+
+const Card = (props) => {
+  switch (props.type) {
+    case "list":
+      return (
+        <CardComp>
+          <Title>{props.title}</Title>
+          <List>
+            {props.list &&
+              props.list.map((listItem, i) => {
+                return (
+                  <ListItem key={i} noBorder={i + 1 === props.list.length}>
+                    {listItem}
+                  </ListItem>
+                );
+              })}
+          </List>
+          {props.buttons &&
+            props.buttons.map((button, i) => {
+              return (
+                <Button
+                  key={i}
+                  secondary={button.secondary}
+                  noMarginTop={button.secondary}
+                  src={button.src}
+                  onClick={button.onClick}
+                >
+                  {button.text}
+                </Button>
+              );
+            })}
+        </CardComp>
+      );
+    case "joinedSalts":
+      return (
+        <CardComp>
+          <Title>{props.title}</Title>
+          <List>
+            {props.list &&
+              props.list.map((listItem, i) => {
+                return (
+                  <ListItem key={i} noBorder={i + 1 === props.list.length}>
+                    {listItem.name}
+                    <SmallButton onClick={listItem.onClick}>Leave</SmallButton>
+                  </ListItem>
+                );
+              })}
+          </List>
+          {props.buttons &&
+            props.buttons.map((button, i) => {
+              return (
+                <Button
+                  key={i}
+                  secondary={button.secondary}
+                  noMarginTop={button.secondary}
+                  src={button.src}
+                  onClick={button.onClick}
+                >
+                  {button.text}
+                </Button>
+              );
+            })}
+        </CardComp>
+      );
+    default:
+      return (
+        <CardComp>
+          <Title text={true}>{props.title}</Title>
+          <Desc>{props.desc}</Desc>
+          {props.buttons.map((button, i) => {
+            return (
+              <Button
+                key={i}
+                secondary={button.secondary}
+                noMarginTop={button.secondary}
+                src={button.src}
+                onClick={button.onClick}
+              >
+                {button.text}
+              </Button>
+            );
+          })}
+        </CardComp>
+      );
+  }
+};
+
+export default Card;
---
+++ client/src/components/SmallButton.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const ButtonComp = styled.button.attrs((props) => ({
+  src: props.src,
+}))`
+  display: block;
+  padding: 6px 8px;
+  text-align: center;
+  background-color: ${(props) => (props.secondary ? "white" : "#b64e1f")};
+  color: ${(props) => (props.secondary ? "#b64e1f" : "white")};
+  margin: 15px 20px;
+  margin-top: ${(props) => props.noMarginTop && "0px"};
+  text-transform: capitalize;
+  border: ${(props) => (props.secondary ? "1px solid #b64e1f" : "none")};
+  outline: none;
+  box-sizing: border-box;
+  font-size: 12px;
+  font-weight: 600;
+  border-radius: 5px;
+  margin: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  transition: background-color 0.2s, color 0.2s, border 0.2s;
+
+  :hover {
+    text-decoration: none;
+    color: ${(props) => (props.secondary ? "#e98455" : "white")};
+    background-color: ${(props) => (props.secondary ? "white" : "#e98455")};
+    border: ${(props) => (props.secondary ? "1px solid #e98455" : "none")};
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  a {
+    width: 100%;
+    height: 100%;
+    color: ${(props) => (props.secondary ? "#b64e1f" : "white")};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+
+    transition: color 0.2s, border 0.2s;
+
+    :hover {
+      text-decoration: none;
+      color: ${(props) => (props.secondary ? "#e98455" : "white")};
+    }
+  }
+`;
+
+const Button = (props) => {
+  return (
+    <ButtonComp
+      secondary={props.secondary}
+      noMarginTop={props.noMarginTop}
+      src={props.src}
+      onClick={props.onClick}
+    >
+      {props.onClick ? (
+        props.children
+      ) : (
+        <Link to={props.src ?? "/"}>{props.children}</Link>
+      )}
+    </ButtonComp>
+  );
+};
+
+export default Button;
