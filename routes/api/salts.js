b29a9b7177ae78c86817a36ef522df8c2972037f
Afuza <theafuza@gmail.com>
2020-11-18 10:51:50 +0700

    remade new salt route to use sequelize association method

--- routes/api/salts.js
+++ routes/api/salts.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 
 const Salt = require("../../models/Salt");
-const User = require("../../models/User");
 
 // @route   POST /api/salts
 // @desc    Create a new salt
@@ -13,33 +12,14 @@ router.post("/", auth, (req, res) => {
   const { name, title, description } = req.body;
   const userId = req.user.id;
 
-  User.findByPk(userId)
-    .then((foundUser) => {
-      if (!foundUser)
-        return res
-          .status(400)
-          .json({ type: "NO_USER", msg: "User does not exist" });
-
-      foundUser.joinedSalts = [name];
-      foundUser
-        .save()
-        .then((newUser) => {
-          Salt.create({
-            name,
-            title,
-            description,
-            members: [userId],
-          })
-            .then((newSalt) => {
-              res.json({ newSalt, newUser });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  Salt.create({
+    name,
+    title,
+    description,
+  })
+    .then((salt) => {
+      salt.addMembers(userId);
+      res.send(salt);
     })
     .catch((err) => {
       console.log(err);
