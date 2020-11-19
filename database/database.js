9021b48b638ad87262190bd6e43f054194a13045
Afuza <theafuza@gmail.com>
2020-11-19 12:00:54 +0700

    disabled logging

--- database/database.js
+++ database/database.js
@@ -6,4 +6,5 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env;
 module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
+  logging: false,
 });
