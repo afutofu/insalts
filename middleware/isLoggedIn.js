cbd641c4e1726330d86212bd67e563e9e2cd8b68
Afuza <theafuza@gmail.com>
2020-11-24 08:35:02 +0700

    renamed auth to isLoggedIn

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,23 +1,21 @@
 require("dotenv/config");
 const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {
+const isLoggedIn = (req, res, next) => {
   const token = req.header("x-auth-token");
 
   // Check for token
   if (!token)
     return res.status(401).json({ msg: "No token, authorization denied" });
 
-  try {
-    // Verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // Verify token
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) res.status(400).json({ msg: "Token is invalid" });
 
-    // Add user from payload
+    // Add info to req.user
     req.user = decoded;
     next();
-  } catch (e) {
-    res.status(400).json({ msg: "Token is invalid" });
-  }
+  });
 };
 
-module.exports = auth;
+module.exports = isLoggedIn;
