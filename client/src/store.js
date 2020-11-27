e73483eba1b9809f3920908821f6f33dc11b9b22
Afuza <theafuza@gmail.com>
2020-11-27 10:41:12 +0700

    develop setup to have redux devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,8 @@ import allReducers from "./store/reducers/";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    allReducers,
-    composeEnhancers(applyMiddleware(thunk))
-    // applyMiddleware(thunk)
+  allReducers,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
