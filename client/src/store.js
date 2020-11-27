95decebd74af5ebe6ac2f826de26d76b0b5ea46e
Afuza <theafuza@gmail.com>
2020-11-27 11:07:09 +0700

    removed redux devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import allReducers from "./store/reducers/";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  allReducers,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 export default store;
