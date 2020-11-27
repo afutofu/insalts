bf20ed6e381aaba32f237c246a168da869656567
Afuza <theafuza@gmail.com>
2020-11-27 10:27:44 +0700

    added DB_PORT to database connection

--- database/database.js
+++ database/database.js
@@ -1,10 +1,18 @@
 const { Sequelize } = require("sequelize");
 require("dotenv/config");
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env;
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_DIALECT,
+  DB_PORT,
+} = process.env;
 
 module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
+  port: DB_PORT,
   logging: false,
 });
