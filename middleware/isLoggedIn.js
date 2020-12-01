0c59c66b34ade32a3582e8c679966ae8b4cac077
Afuza <theafuza@gmail.com>
2020-12-01 09:50:15 +0700

    added documentation

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,18 +1,26 @@
 require("dotenv/config");
 const jwt = require("jsonwebtoken");
 
+/*
+isLoggedIn
+
+Checks if the user is logged in
+Uses jwt verification 
+*/
 const isLoggedIn = (req, res, next) => {
+  // Get token from headers
   const token = req.header("x-auth-token");
 
-  // Check for token
+  // If token does not exist, return error
   if (!token)
     return res.status(401).json({ msg: "No token, authorization denied" });
 
   // Verify token
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    // If error exists, means token is invalid, return error
     if (err) return res.status(400).json({ msg: "Token is invalid" });
 
-    // Add info to req.user
+    // Assign decoded information to req.user
     req.user = decoded;
     next();
   });
